1f0ec6caeebe52b7151338c229278e81eb315d7d
Bikashkhanal130 <khanalbk18@gmail.com>
2023-12-07 08:24:17 +0545

    add new

---
+++ Day-03/main.js
@@ -0,0 +1,15 @@
+a=5;
+a="hello";
+a=false;
+
+roll =[2,4,6,8,9];
+
+
+subjects=[23,45,76,87,12];
+console.log(subjects);
+subjects={'math':23,'com':45,'eng':76,'social':87,'nepali':12};
+console.log(subjects);
+console.log(typeof roll);
+
+console.log(roll);
+console.log(roll[3]);
